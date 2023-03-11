d8951b960ae9427ab01252cd282bf95f16142394
Justin Fulton <jusfulton@unsubstantiatedscript.com>
2023-03-10 18:51:24 -0800

    yeet this

--- api/account.go
+++ api/account.go
@@ -1,1 +1,20 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+//Only using these params as every account will start with a zero balance
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required, oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+}
--- api/server.go
+++ api/server.go
@@ -1,1 +1,24 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+// Serves up all HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+//NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	//Setting things up
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// add routes to router
+
+	server.router = router
+	return server
+}
